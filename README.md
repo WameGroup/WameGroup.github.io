974690b8819116085abed68c0de6080081ba8945
= <=>
2019-08-13 15:31:02 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Wame
+
+### Web Page of Wame Hub
+
+Landing page dedicated to the promotion and information of the Wame freelance work group.
---
+++ js/funciones.js
@@ -0,0 +1,44 @@
+
+//--------------Documento de funciones javascript para la interaccion------------//
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+var index = 0;
+setInterval(function () {
+  if (index == 0) {
+    let panel = document.getElementById('primer-panel');
+    panel.classList.add('ocultar');
+    let panel1 = document.getElementById('segundo-panel');
+    panel1.classList.remove('ocultar');
+    index = 1;
+  }else if(index==1){
+    let panel = document.getElementById('primer-panel');
+    panel.classList.remove('ocultar');
+    let panel1 = document.getElementById('segundo-panel');
+    panel1.classList.add('ocultar');
+    index = 0;
+  }
+
+}, 5000);
